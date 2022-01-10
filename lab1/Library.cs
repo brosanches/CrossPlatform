9b1c075a634d06ce06e80620355d64b4c16ce50f
Tony S <71261168+brosanches@users.noreply.github.com>
2022-01-10 08:16:08 +0200

    Add files via upload

---
+++ lab1/Library.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public static class Library
+    {
+        private static StringBuilder a;
+        private static StringBuilder b;
+
+        private static int[] black = { 6, 2, 5, 5, 4, 5, 6, 3, 7, 6 };
+
+        private static bool CheckN(int x) => !(1 <= x && x <= 100);
+        private static bool CheckK(int x) => !(1 <= x && x <= 700);
+        private static int ConvertToInt32(string value)
+        {
+            bool parsed = Int32.TryParse(value, out int result);
+
+            if (!parsed)
+                throw new ArgumentException($"The value {value} was not parsed to int");
+
+            return result;
+        }
+        public static void ParseStrings(string[] lines, out int N, out int K)
+        {
+            N = ConvertToInt32(lines[0].Split()[0]);
+            K = ConvertToInt32(lines[0].Split()[1]);
+
+            if (CheckN(N))
+                throw new ArgumentException("Input values do not match criteria 1 <= N <= 100");
+
+            if (CheckK(K))
+                throw new ArgumentException($"Input values do not match criteria 1 <= K <= 700");
+        }
+
+        public static string GetMin (int N, int K, string prefix = "")
+        {
+            if (prefix.Length == N)
+                a = new StringBuilder(prefix);
+
+            else if (prefix.Length == 0) 
+            {
+                if (K < 2 * N || K > 7 * N)
+                    a = new StringBuilder("NO SOLUTION");
+                else 
+                {
+                    int j = 1;
+                    bool Stop = false;
+                    while (!Stop && j < 10)
+                    {
+                        if (2 * (N - (prefix.Length + 1)) <= K - black[j] && K - black[j] <= 7 * (N - (prefix.Length + 1)))
+                        {
+                            GetMin(N, K - black[j], prefix + j);
+                            Stop = true; 
+                        }
+                        j += 1;
+                    }
+                }
+            }
+            else
+            {
+                int j = 0;
+                bool Stop = false;
+
+                while (!Stop && j < 10)
+                {
+                    if (2 * (N - (prefix.Length + 1)) <= K - black[j] && K - black[j] <= 7 * (N - (prefix.Length + 1)))
+                    {
+                        GetMin(N, K - black[j], prefix + j);
+                        Stop = true;
+                    }
+                    j += 1;
+                }
+            }
+
+            return a.ToString();
+        }
+
+        public static string GetMax(int N, int K, string prefix = "")
+        {
+            if (prefix.Length == N)
+                return prefix;
+
+            else if (prefix.Length == 0)
+            {
+                if (K < 2 * N || K > 7 * N)
+                    return "NO SOLUTION";
+                else
+                {
+                    int j = 9;
+                    while (j > 0)
+                    {
+                        if (2 * (N - (prefix.Length + 1)) <= K - black[j] && K - black[j] <= 7 * (N - (prefix.Length + 1)))
+                        {
+                            return GetMax(N, K - black[j], prefix + j);
+                        }
+                        j -= 1;
+                    }
+                }
+            }
+            else
+            {
+                int j = 9;
+
+                while (j > -1)
+                {
+                    if (2 * (N - (prefix.Length + 1)) <= K - black[j] && K - black[j] <= 7 * (N - (prefix.Length + 1)))
+                    {
+                        return GetMax(N, K - black[j], prefix + j);
+                    }
+                    j -= 1;
+                }
+            }
+
+            return prefix;
+        }
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Numerics;
+using System.Linq;
+
+namespace lab1
+{
+    public class Program
+    {
+        [Option(ShortName = "i")]
+        public string InputFile { get; }
+
+        [Option(ShortName = "o")]
+        public string OutputFile { get; }
+
+        static void Main(string[] args)
+            => CommandLineApplication.Execute<Program>(args);
+
+        private void OnExecute()
+        {
+            string[] lines = File.ReadAllLines(InputFile);
+
+            int N, K;
+
+            Library.ParseStrings(lines, out N, out K);
+
+            string min = Library.GetMin(N, K);
+
+            string max = Library.GetMax(N, K);
+
+            string res = "";
+
+            if (String.Equals(min, "NO SOLUTION") && String.Equals(max, "NO SOLUTION"))
+            {
+                res = "NO SOLUTION";
+            }
+            else
+            {
+                if (!String.Equals(min, "NO SOLUTION"))
+                {
+                    res += min;
+                }
+                if (!String.Equals(max, "NO SOLUTION"))
+                {
+                    res += "\n" + max;
+                }
+            }
+
+            Console.WriteLine(res);
+
+            File.WriteAllText(OutputFile, res);
+        }
+    }
+}
